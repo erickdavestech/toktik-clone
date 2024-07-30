b8d34f98081a7ad40ed5808e9de847fdfdfe7625
erickdavestech <erickdavestech@gmail.com>
2024-07-30 04:14:16 -0400

    _bases_for_config_

---
+++ lib/Ui/Pages/toktik_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TokTikPage extends StatelessWidget {
+  const TokTikPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Text('Hola'));
+  }
+}
