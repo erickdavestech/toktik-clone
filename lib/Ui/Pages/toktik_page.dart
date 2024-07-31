4c36bbca506fc7d78432273cf905e3455b82651d
x <erickdavestech@gmail.com>
2024-07-31 04:34:22 -0700

    toktik_CircularProgressIndicator

--- lib/Ui/Pages/toktik_page.dart
+++ lib/Ui/Pages/toktik_page.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:toktik_clone/Providers/provider_videos.dart';
 
 class TokTikPage extends StatelessWidget {
   const TokTikPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: Center(
-            child: Text(
-      'TokTik Vertical Videos',
-      textAlign: TextAlign.center,
-      style: TextStyle(
-        fontSize: 80,
-        fontWeight: FontWeight.bold,
-      ),
-    )));
+    //
+    final videoProv = context.watch<VideoProvider>();
+
+    return Scaffold(
+        body: videoProv.initialLoading
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : const Placeholder());
   }
 }
